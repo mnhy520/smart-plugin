958a3471b06ae9b1705b5d4beacdecc4f6522e5f
gulx <470112056@qq.com>
2017-05-04 20:39:40 +0800

    js插件

---
+++ src/myPlugin.js
@@ -0,0 +1,35 @@
+;(function($, window, document,undefined) {
+    //定义Beautifier的构造函数
+    var Beautifier = function(ele, opt) {
+        this.$element = ele,
+        this.defaults = {
+            'color': 'red',
+            'fontSize': '12px',
+            'textDecoration': 'none',
+            'click':function(){}
+        },
+        this.options = $.extend({}, this.defaults, opt);
+        
+        this.addListner();
+    }
+    //定义Beautifier的方法
+    Beautifier.prototype = {
+        beautify: function() {
+            return this.$element.css({
+                'color': this.options.color,
+                'fontSize': this.options.fontSize,
+                'textDecoration': this.options.textDecoration
+            });
+        },
+        addListner:function(){
+        	this.$element.click(this.options.click);
+        }
+    }
+    //在插件中使用Beautifier对象
+    $.fn.myPlugin = function(options) {
+        //创建Beautifier的实体
+        var beautifier = new Beautifier(this, options);
+        //调用其方法
+        return beautifier.beautify();
+    }
+})(jQuery, window, document);
